9d39957712fc42436cfa4ea9e375d92ef30101d0
JMartinTorres <jmartintorres@outlook.es>
2020-04-04 19:51:15 +0200

    ActiveTab and tabs permissions removed | Mouseover + Hover = Houseover, the Ultimate Mouse Detector (probably not)

--- youtube_first_playlist_video/content_scripts/context_menu_requests.js
+++ youtube_first_playlist_video/content_scripts/context_menu_requests.js
@@ -1,33 +1,6 @@
 $( document ).ready(function() {
 	
-	var optionsInMenu = false;
-	
-	function checkFollowedChannelsDivIsLoaded() {
-		
-		// Mutation Observer instantiation
-		var mutationObs = new MutationObserver(callbackChannelAdded);
-		
-		// Observe initialization
-		mutationObs.observe(document.body, { childList: true, subtree: true });
-	  
-	}
-
-	function callbackChannelAdded(mutations, mutationObs) {
-		
-		// Rambo programming 💣🔫
-		$("a[href*='&list']") 
-		// .hover(addMenuOptions, removeMenuOptions)
-		.mouseover(addMenuOptions)
-		.mouseout(removeMenuOptions)
-		.focusin(addMenuOptions)
-		.focusout(removeMenuOptions)
-		
-	}
-	
-	// Initialize MutationObserver
-	checkFollowedChannelsDivIsLoaded();
-	
-	// MouseOver and focusIn events: send message to background script to add the context menu options
+	// MouseOver, HoverIn and focusIn events: send message to background script to add the context menu options
 	var addMenuOptions = function () {
 		
 		if (!$(this).data("handler")) {
@@ -35,7 +8,7 @@ $( document ).ready(function() {
 			
 			chrome.runtime.sendMessage(
 				{action: "addContextMenuOptions", url: window.origin + $(this).attr('href').substring(0,$(this).attr('href').indexOf("&list"))},
-				function (response) {
+				function () {
 					return true;
 				}
 			);
@@ -44,14 +17,14 @@ $( document ).ready(function() {
 		
 	};
 	
-	// MouseOut and focusOut events: send message to background script to remove the added context menu options
+	// MouseOut, HoverOut and focusOut events: send message to background script to remove the added context menu options
 	var removeMenuOptions = function () {
 		if ($(this).data("handler")) {
 			$(this).removeData('handler');
 			
 			chrome.runtime.sendMessage(
 				{action: "removeContextMenuOptions"},
-				function (response) {
+				function () {
 					return true;
 				}
 			);
@@ -59,6 +32,24 @@ $( document ).ready(function() {
 		};
 		
 	};
+
+	// MutationObserver instantiation
+	var mutationObs = new MutationObserver(function (mutations) { 
+		addOrRemoveOptionsOnMouseInteraction();
+	});
+		
+	// Observe initialization
+	mutationObs.observe(document.body, { childList: true, subtree: true });
+
+	function addOrRemoveOptionsOnMouseInteraction () {
+		// Rambo programming 💣🔫
+        $("a[href*='&list']") 
+		.hover(addMenuOptions, removeMenuOptions)
+		.mouseover(addMenuOptions)
+		.mouseout(removeMenuOptions)
+		.focusin(addMenuOptions)
+		.focusout(removeMenuOptions)
+	}
 	
 });
 
