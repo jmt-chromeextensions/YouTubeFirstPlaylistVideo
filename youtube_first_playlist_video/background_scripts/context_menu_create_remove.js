b5123702de66af1d0bde3fa33d4274e8c113c850
JMartinTorres <jmartintorres@outlook.es>
2020-04-19 18:32:13 +0200

    Added one more option to allow to open the link in a new tab and switch to it or stay in the current one

--- youtube_first_playlist_video/background_scripts/context_menu_create_remove.js
+++ youtube_first_playlist_video/background_scripts/context_menu_create_remove.js
@@ -1,72 +1,92 @@
 // Inbox 📫
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-		
+	function (request) {
+
 		if (request.action === "addContextMenuOptions") {
 			// Parent: 'Open video in a...'
 			chrome.contextMenus.create(
-			{"title": "Open first video in...",
-			"id": 'ynl_context_menu_parent',
-			"contexts": ["link"],
-			"documentUrlPatterns":["https://*.youtube.com/*"]
-			});
-			
-			// New tab
+				{
+					"title": "Open first video in...",
+					"id": 'ynl_context_menu_parent',
+					"contexts": ["link"],
+					"documentUrlPatterns": ["https://*.youtube.com/*"]
+				});
+
+			// New tab (no switch)
 			chrome.contextMenus.create(
-			{"title": "new tab",
-			"parentId": "ynl_context_menu_parent",
-			"id": 'ynl_context_menu_tab',
-			"contexts": ["link"],
-			"documentUrlPatterns":["https://*.youtube.com/*"],
-			"onclick": () => openVideo("openVideoNewTab", request.url)
-			});
-			
+				{
+					"title": "new tab",
+					"parentId": "ynl_context_menu_parent",
+					"id": 'ynl_context_menu_tab',
+					"contexts": ["link"],
+					"documentUrlPatterns": ["https://*.youtube.com/*"],
+					"onclick": () => openVideo("openVideoNewTab", request.url)
+				});
+
+			// New tab (switch)
+			chrome.contextMenus.create(
+				{
+					"title": "new tab (switch)",
+					"parentId": "ynl_context_menu_parent",
+					"id": 'ynl_context_menu_tab_switch',
+					"contexts": ["link"],
+					"documentUrlPatterns": ["https://*.youtube.com/*"],
+					"onclick": () => openVideo("openVideoNewTabSwitch", request.url)
+				});
+
 			// New window
 			chrome.contextMenus.create(
-			{"title": "new window",
-			"parentId": "ynl_context_menu_parent",
-			"id": 'ynl_context_menu_window',
-			"contexts": ["link"],
-			"documentUrlPatterns":["https://*.youtube.com/*"],
-			"onclick": () => openVideo("openVideoNewWindow", request.url)
-			});
-			
+				{
+					"title": "new window",
+					"parentId": "ynl_context_menu_parent",
+					"id": 'ynl_context_menu_window',
+					"contexts": ["link"],
+					"documentUrlPatterns": ["https://*.youtube.com/*"],
+					"onclick": () => openVideo("openVideoNewWindow", request.url)
+				});
+
 			// New incognito window
 			chrome.contextMenus.create(
-			{"title": "incognito window",
-			"parentId": "ynl_context_menu_parent",
-			"id": 'ynl_context_menu_incognito',
-			"contexts": ["link"],
-			"documentUrlPatterns":["https://*.youtube.com/*"],
-			"onclick": () => openVideo("openVideoIncognitoWindow", request.url)
-			});
-			
+				{
+					"title": "incognito window",
+					"parentId": "ynl_context_menu_parent",
+					"id": 'ynl_context_menu_incognito',
+					"contexts": ["link"],
+					"documentUrlPatterns": ["https://*.youtube.com/*"],
+					"onclick": () => openVideo("openVideoIncognitoWindow", request.url)
+				});
+
 		}
-			
-		else if (request.action === "removeContextMenuOptions") 
+
+		else if (request.action === "removeContextMenuOptions")
 			chrome.contextMenus.remove('ynl_context_menu_parent');
-		
-		sendResponse("bar");
-		
-    }
+
+	}
 );
 
 function openVideo(openMode, url) {
-	
+
 	// 'Open video in new tab' option clicked 
 	if (openMode === 'openVideoNewTab') {
+		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+			chrome.tabs.create({ url: url, active: false, index: (tabs[0].index + 1) });
+        });
+	}
+
+	// 'Open video in new tab and switch to it' option clicked
+	else if (openMode === 'openVideoNewTabSwitch') {
 		window.open(url);
-	} 
-	
+	}
+
 	// 'Open video in new window' option clicked
 	else if (openMode === 'openVideoNewWindow') {
-		chrome.windows.create({"url": url, "state": "maximized"});
-	} 
-	
+		chrome.windows.create({ "url": url, "state": "maximized" });
+	}
+
 	// 'Open video in incognito window'
 	else if (openMode === 'openVideoIncognitoWindow') {
-		chrome.windows.create({"url": url, "incognito": true, "state": "maximized"});
-	} 
-	
+		chrome.windows.create({ "url": url, "incognito": true, "state": "maximized" });
+	}
+
 }
 
