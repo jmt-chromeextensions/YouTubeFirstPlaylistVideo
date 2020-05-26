ad7c5b02152a7e9ecb2649a294da1264adec1b98
JMartinTorres <jmartintorres@outlook.es>
2020-05-26 22:38:42 +0200

    HTML title and minor visual adjustments

--- youtube_first_playlist_video/settings/settings.js
+++ youtube_first_playlist_video/settings/settings.js
@@ -4,7 +4,9 @@ $(document).ready(function() {
 	chrome.storage.sync.get('optionsChoice', function(result) {
         // Check if some value is stored
         if (result.optionsChoice)
-            $("#" + result.optionsChoice).prop('checked', true);
+            setTimeout(() => {
+                $("#" + result.optionsChoice).prop('checked', true);
+            }, 150); // A little timeout to let the user appreciate the beatiful animation
         else
             $("#0").prop('checked', true)
 	});
