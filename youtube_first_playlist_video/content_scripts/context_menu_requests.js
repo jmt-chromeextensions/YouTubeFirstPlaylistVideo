d0f77b74d41da80b573f91c95eaf97f8a9075425
JMartinTorres <jmartintorres@outlook.es>
2021-02-06 16:31:16 +0100

    Constants and comments

--- youtube_first_playlist_video/content_scripts/context_menu_requests.js
+++ youtube_first_playlist_video/content_scripts/context_menu_requests.js
@@ -1,18 +1,28 @@
+CONST PARTIAL_HREF_LIST_FIRST_PARAM = "watch?list";
+CONST PARTIAL_HREF_LIST_SUBSEQUENT_PARAM = "&list";
+CONST PLAYLIST_CONTENT_DIV = "#content.ytd-playlist-video-renderer";
+
+/* In playlists' context, handlers are assigned to anchors contained in divs that are also are assigned the handlers. 
+Therefore, the context menu options addition message could be sent twice before any 'mouseleave' handler is triggered. (inOut = 2).
+In this situation, we want to avoid the removal message to be sent until the 'mouseleave' event has been triggered twice. (inOut = 0) */
 var inOut = 0;
 
-// MouseOver, HoverIn and focusIn events: send message to background script to add the context menu options
+// MouseEnter event: send message to background script to add the context menu options
 function addMenuOptions (e) {
 	inOut++;
 
+	/* This href processing is done here because YouTube reuses its HTML elements when the user navigates from one page to another.
+	Elements may have a specific href in one moment and a different one in another moment. */
+
 	let href = $(this).attr("href") ? $(this).attr("href") : $(this).find("a[href*='list']").attr("href"); // Div containing anchor | Anchor
-	let hrefNoList = href.includes("&list") ? 
-	href.substring(0, href.indexOf("&list")) : // href="/watch?v=uSG4tG40Wb8&list=PLp3-KQ922RELlU_q8bmxEMtweK_b8jo0j&index=122"
+	let hrefNoList = href.includes(PARTIAL_HREF_LIST_SUBSEQUENT_PARAM) ? 
+	href.substring(0, href.indexOf(PARTIAL_HREF_LIST_SUBSEQUENT_PARAM)) : // href="/watch?v=uSG4tG40Wb8&list=PLp3-KQ922RELlU_q8bmxEMtweK_b8jo0j&index=122"
 	`/watch?v=${href.substring(href.indexOf("v=") + 2)}` ; // href="https://www.youtube.com/watch?list=PLp3-KQ922RELlU_q8bmxEMtweK_b8jo0j&v=_Df15XQgyq4"
 		
 	chrome.runtime.sendMessage({ action: "addContextMenuOptions", url: window.origin + hrefNoList });
 };
 
-// MouseOut, HoverOut and focusOut events: send message to background script to remove the added context menu options
+// MouseLeave event: send message to background script to remove the added context menu options
 function removeMenuOptions () {
 	inOut--;
 
@@ -30,10 +40,10 @@ function addOrRemoveOptionsOnMouseInteraction (e) {
 $(document).ready(function() {
 	
 	// Initial handlers assignment
-	$("a[href*='&list'], a[href*='watch?list']").each(function () {
+	$(`a[href*='${PARTIAL_HREF_LIST_SUBSEQUENT_PARAM}'], a[href*='${PARTIAL_HREF_LIST_FIRST_PARAM}']`).each(function () {
 
 		// Playlist div
-		let contentDiv = $(this).closest("#content.ytd-playlist-video-renderer").not(".playlist-context-menu");
+		let contentDiv = $(this).closest(PLAYLIST_CONTENT_DIV).not(".playlist-context-menu");
 		if (contentDiv.length > 0)
 			addOrRemoveOptionsOnMouseInteraction(contentDiv);
 
@@ -43,10 +53,10 @@ $(document).ready(function() {
 
 	// MutationObserver instantiation
 	var mutationObs = new MutationObserver(function () {
-		$("a[href*='&list'], a[href*='watch?list']").not(".playlist-context-menu").each(function () {
+		$(`a[href*='${PARTIAL_HREF_LIST_SUBSEQUENT_PARAM}'], a[href*='${PARTIAL_HREF_LIST_FIRST_PARAM}']`).not(".playlist-context-menu").each(function () {
 
 			// Playlist div
-			let contentDiv = $(this).closest("#content.ytd-playlist-video-renderer").not(".playlist-context-menu");
+			let contentDiv = $(this).closest(PLAYLIST_CONTENT_DIV).not(".playlist-context-menu");
 			if (contentDiv.length > 0)
 				addOrRemoveOptionsOnMouseInteraction(contentDiv);
 
